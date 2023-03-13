de2f9b29033071bc04cb3de34b717f5a5bfdb159
Sinton <yansuntong@gmail.com>
2023-03-14 01:05:01 +0800

    import project

---
+++ Sources/obd-api-swift/obd_api_swift.swift
@@ -0,0 +1,6 @@
+public struct obd_api_swift {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/obd-api-swiftTests/obd_api_swiftTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import obd_api_swift
+
+final class obd_api_swiftTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(obd_api_swift().text, "Hello, World!")
+    }
+}
